378f82cfd7eee371e58958cc29dd7019934cf2aa
NomanKhanbangash <nomanbangash@gmail.com>
2025-06-11 10:59:11 +0500

    file udpate

--- cypress/e2e/features/users.feature
+++ cypress/e2e/features/users.feature
@@ -1,19 +1,40 @@
+# Feature: GoRest Users API
+
+#   Scenario: Get all users
+#     When I fetch all users
+#     Then the response status should be 200
+
+#   Scenario: Create a new user
+#     When I create a user with random email
+#     Then the user should be created successfully
+
+#   Scenario: Get user by ID
+#     Given a user exists
+#     When I fetch the user by ID
+#     Then the correct user details are returned
+
+#   Scenario: Update a user
+#     Given a user exists
+#     When I update the user's name
+#     Then the user's name should be updated
+
+
 Feature: GoRest Users API
 
   Scenario: Get all users
-    When I fetch all users
+    When fetch all users
     Then the response status should be 200
 
-  Scenario: Create a new user
-    When I create a user with random email
+  Scenario: Create a new user with random email
+    When Create a user with name "Test User" and gender "male" and status "active"
     Then the user should be created successfully
 
   Scenario: Get user by ID
-    Given a user exists
-    When I fetch the user by ID
+    Given a user with name "Test User" and gender "male" and status "active" exists
+    When fetch the user by ID
     Then the correct user details are returned
 
   Scenario: Update a user
-    Given a user exists
-    When I update the user's name
+    Given a user with name "Test User" and gender "male" and status "active" exists
+    When Update the user's name to "Updated User"
     Then the user's name should be updated
--- cypress/support/step_definitions/usersSteps.js
+++ cypress/support/step_definitions/usersSteps.js
@@ -4,7 +4,7 @@ import usersApi from "../api/usersApi";
 let createdUser = null;
 let response = null;
 
-When("I fetch all users", () => {
+When("fetch all users", () => {
   usersApi.getUsers().then((res) => {
     response = res;
   });
@@ -14,39 +14,46 @@ Then("the response status should be {int}", (status) => {
   expect(response.status).to.eq(status);
 });
 
-When("I create a user with random email", () => {
-  const random = Math.random().toString(36).substring(2, 7);
-  const user = {
-    name: "Test User",
-    gender: "male",
-    email: `testuser_${random}@mail.com`,
-    status: "active",
-  };
-  usersApi.createUser(user).then((res) => {
-    response = res;
-    createdUser = res.body;
-  });
-});
+When(
+  "Create a user with name {string} and gender {string} and status {string}",
+  (name, gender, status) => {
+    const random = Math.random().toString(36).substring(2, 7);
+    const user = {
+      name,
+      gender,
+      email: `testuser_${random}@mail.com`,
+      status,
+    };
+    usersApi.createUser(user).then((res) => {
+      response = res;
+      createdUser = res.body;
+    });
+  }
+);
 
 Then("the user should be created successfully", () => {
   expect(response.status).to.eq(201);
   expect(createdUser).to.have.property("email");
+  expect(createdUser).to.have.property("name");
 });
 
-Given("a user exists", () => {
-  const random = Math.random().toString(36).substring(2, 7);
-  const user = {
-    name: "Test User",
-    gender: "male",
-    email: `testuser_${random}@mail.com`,
-    status: "active",
-  };
-  usersApi.createUser(user).then((res) => {
-    createdUser = res.body;
-  });
-});
+Given(
+  "a user with name {string} and gender {string} and status {string} exists",
+  (name, gender, status) => {
+    const random = Math.random().toString(36).substring(2, 7);
+    const user = {
+      name,
+      gender,
+      email: `testuser_${random}@mail.com`,
+      status,
+    };
+    usersApi.createUser(user).then((res) => {
+      createdUser = res.body;
+    });
+  }
+);
 
-When("I fetch the user by ID", () => {
+When("fetch the user by ID", () => {
   usersApi.getUserById(createdUser.id).then((res) => {
     response = res;
   });
@@ -57,13 +64,13 @@ Then("the correct user details are returned", () => {
   expect(response.body.id).to.eq(createdUser.id);
 });
 
-When("I update the user's name", () => {
-  usersApi.updateUser(createdUser.id, { name: "Updated User" }).then((res) => {
+When("Update the user's name to {string}", (newName) => {
+  usersApi.updateUser(createdUser.id, { name: newName }).then((res) => {
     response = res;
   });
 });
 
 Then("the user's name should be updated", () => {
   expect(response.status).to.eq(200);
-  expect(response.body.name).to.eq("Updated User");
+  expect(response.body.name).to.not.eq(createdUser.name);
 });
